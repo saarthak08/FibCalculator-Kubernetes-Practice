9950976c9dcab3b0c78e09e8a278dbc882afd68d
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-02-22 18:34:11 +0530

    add files

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      Im some other page!
+      <br></br>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
